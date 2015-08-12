3f81e6c223c1ef8f84f5a5bcdd6c493ebf890c40
Udit Agarwal <uditiiita@gmail.com>
2015-08-13 03:00:14 +0530

    Remove attribution

--- footer.php
+++ footer.php
@@ -141,8 +141,6 @@
 						echo esc_attr($zerif_copyright);
 					endif;
 					
-					echo '<div class="zerif-copyright-box"><a class="zerif-copyright" href="http://themeisle.com/themes/zerif-lite/" target="_blank" rel="nofollow">Zerif Lite </a>'.__('powered by','zerif-lite').'<a class="zerif-copyright" href="http://wordpress.org/" target="_blank" rel="nofollow"> WordPress</a></div>';
-					
 					echo '</div>';
 			
 		endif;
